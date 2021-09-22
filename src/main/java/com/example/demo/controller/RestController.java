b0df91b385e01be4b6f4c550efeb62e35b206b0b
Amresh <amrsh.kmr@gmail.com>
2021-09-22 19:00:32 +0530

    pathvariable method added

--- src/main/java/com/example/demo/controller/RestController.java
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -5,26 +5,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
-	
-	
-	@GetMapping(path="/hello-world")
+
+	@GetMapping(path = "/hello-world")
 	public String HelloWorld() {
-		return "Hello World"; 
-		
-		
+		return "Hello World";
+
 	}
-	
-	@GetMapping(path="/hello-world-bean")
+
+	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean HelloWorldBean() {
-		return new HelloWorldBean("Hello World"); 
-		
-		
+		return new HelloWorldBean("Hello World");
+
 	}
-	
-	@GetMapping(path="/hello-world-pathVariable/{name}")
+
+	@GetMapping(path = "/hello-world-pathVariable/{name}")
 	public HelloWorldBean HelloWorldPathVariable(@PathVariable String name) {
-		return new HelloWorldBean("Hello World :  "+name); 
-		
-		
+		return new HelloWorldBean("Hello World :  " + name);
+
 	}
 }
