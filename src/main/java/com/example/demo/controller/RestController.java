f6df2ffb4e033f5634d658eb6ffe8320c24ac248
Amresh <amrsh.kmr@gmail.com>
2021-09-22 18:11:12 +0530

    Hello world controller added

---
+++ src/main/java/com/example/demo/controller/RestController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+	
+	
+	@GetMapping(path="/hello-world")
+	public String HelloWorld() {
+		return "Hello World";
+		
+		
+	}
+}
