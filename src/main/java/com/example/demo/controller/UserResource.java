25f43a326c408ec401cdd42ae7ccf5ad9c31234e
Amresh <amrsh.kmr@gmail.com>
2021-09-23 12:23:49 +0530

    Response Entity added for post method

--- src/main/java/com/example/demo/controller/UserResource.java
+++ src/main/java/com/example/demo/controller/UserResource.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.controller.user.User;
 import com.example.demo.controller.user.UserDaoService;
@@ -52,8 +55,12 @@ public class UserResource {
 	 * @return the user
 	 */
 	@PostMapping("/users")
-	public User addUsers(@RequestBody User user){
-		return service.save(user);
+	public ResponseEntity<Object> addUsers(@RequestBody User user){
+		User savedUser= service.save(user);
+		
+		URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("{id}").buildAndExpand(savedUser.getId()).toUri();
+		
+		return ResponseEntity.created(location).build();
 	}
 
 }
